9c1d3d8d05c20758f969fb96a3b290ddf8150b7f
Bonface Mudiri <mudirib@gmail.com>
2020-01-09 23:09:24 -0800

    Create App Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup)
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('should render without crashing', () => {
+  const div = document.createElement("div");
+  render(<App />, div);
 });
+
+test('should render h1', () => {
+  const { getByText } = render(<App />)
+  const text = getByText('Hello World');
+  expect(text).toBeInTheDocument();
+})
+
